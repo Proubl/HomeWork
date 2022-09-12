0000b5e160985d03f95bf8dd32a43d2bc58972d5
Proubl <112727567+Proubl@users.noreply.github.com>
2022-09-12 23:16:06 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+main() {
+    setlocale(LC_ALL, "RUS");
+    puts("********************");
+    puts("*         *        *");
+    puts("*        * *       *");
+    puts("*       *   *      *");
+    puts("*      *     *     *");
+    puts("*      *     *     *");
+    puts("*      *     *     *");
+    puts("*      *******     *");
+    puts("********************");
+    return 0;
+}
